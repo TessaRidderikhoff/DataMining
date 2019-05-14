87ba7d0e59c72bf7716cae219d44884dd898eb68
Nicole <Nicole-de-groot@users.noreply.github.com>
2019-05-14 19:50:10 +0200

    Letor & LambdaMART
    Werkt alleen nog niet...

---
+++ hotels/LambdaMart/LambdaMART.py
@@ -0,0 +1,30 @@
+import pyltr
+
+with open('train.txt') as trainfile, \
+        open('test.txt') as evalfile:
+    TX, Ty, Tqids, _ = pyltr.data.letor.read_dataset(trainfile)
+    EX, Ey, Eqids, _ = pyltr.data.letor.read_dataset(evalfile)
+
+metric = pyltr.metrics.NDCG(k=5)
+
+# Only needed if you want to perform validation (early stopping & trimming)
+#monitor = pyltr.models.monitors.ValidationMonitor(
+#    VX, Vy, Vqids, metric=metric, stop_after=250)
+
+model = pyltr.models.LambdaMART(
+    metric=metric,
+    n_estimators=1000,
+    learning_rate=0.02,
+    max_features=0.5,
+    query_subsample=0.5,
+    max_leaf_nodes=10,
+    min_samples_leaf=64,
+    verbose=1,
+)
+
+model.fit(TX, Ty, Tqids)
+#model.fit(TX, Ty, Tqids, monitor=monitor)
+
+Epred = model.predict(EX)
+print ('Random ranking:', metric.calc_mean_random(Eqids, Ey))
+print ('Our model:', metric.calc_mean(Eqids, Ey, Epred))
