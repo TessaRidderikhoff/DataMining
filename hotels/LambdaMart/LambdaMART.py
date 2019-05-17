d19a2b388190fdb2a3211212e9d09a2e9ab38d55
Nicole <Nicole-de-groot@users.noreply.github.com>
2019-05-17 12:46:43 +0200

    letor & LambdaMart
    and deleted temporary files

--- hotels/LambdaMart/LambdaMART.py
+++ hotels/LambdaMart/LambdaMART.py
@@ -1,10 +1,19 @@
 import pyltr
 import pandas as pd
+import numpy as np
+import os
 
-with open('train.txt') as trainfile, \
-        open('test.txt') as evalfile:
+train_file = 'test_files/train.txt'
+test_file = 'test_files/test.txt'
+
+with open(train_file) as trainfile, \
+        open(test_file) as evalfile:
+    print('opening and reading train file...')
     TX, Ty, Tqids, _ = pyltr.data.letor.read_dataset(trainfile)
+    print('train file succesfully read.')
+    print('opening and reading test file...')
     EX, Ey, Eqids, _ = pyltr.data.letor.read_dataset(evalfile)
+    print('test file succesfully read.')
 
 metric = pyltr.metrics.NDCG(k=5, gain_type='identity')
 
@@ -12,6 +21,7 @@ metric = pyltr.metrics.NDCG(k=5, gain_type='identity')
 #monitor = pyltr.models.monitors.ValidationMonitor(
 #    VX, Vy, Vqids, metric=metric, stop_after=250)
 
+print('creating model...')
 model = pyltr.models.LambdaMART(
     metric=metric,
     n_estimators=10,
@@ -23,18 +33,32 @@ model = pyltr.models.LambdaMART(
     verbose=1,
 )
 
+print('fitting model...')
 model.fit(TX, Ty, Tqids)
 #model.fit(TX, Ty, Tqids, monitor=monitor)
 
+print('predicting model...')
 Epred = model.predict(EX)
-print(Epred)
 print ('Random ranking:', metric.calc_mean_random(Eqids, Ey))
 print ('Our model:', metric.calc_mean(Eqids, Ey, Epred))
 
+print('creating result file...')
 result = pd.DataFrame({'srch_id' : Eqids,
                         'prop_id' : Ey,
                         'score' : Epred})
 
+print('sorting results...')
 result = result.sort_values(by=['srch_id', 'score'], ascending=[True, False])
 
-result.to_csv('results.csv')
+result['prop_id'] = result['prop_id'].astype(np.int64)
+
+i = 1
+while os.path.isfile('results/results_%s.csv' % str(i)):
+    i += 1
+result.to_csv('results/results_%s.csv' % str(i), index=False, header=True)
+print('result file succesfully created.')
+
+results = result.drop(axis=1, columns=['score'])
+
+results.to_csv('results/submission_%s.csv' % str(i), index=False, header=True)
+print('submission file succesfully created.')
